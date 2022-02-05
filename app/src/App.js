cbe69627f13df2b9f6e72a60de350079a8baffcd
leovct <l009.vincent@gmail.com>
2022-02-05 19:48:20 +0100

    feat: metamask auth

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert('Make sure you have Metamask!');
+      return;
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    if (accounts.length === 0) {
+      //alert('No authorized account found');
+      return;
+    }
+    const account = accounts[0];
+    setCurrentAccount(account);
+
+    // Setup listener! This is for the case where a user comes to our site
+    // and ALREADY had their wallet connected + authorized.
+    setupEventListener();
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Make sure you have Metamask!');
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      if (accounts.length === 0) {
+        //alert('No authorized account found');
+        return;
+      }
+      const account = accounts[0];
+      setCurrentAccount(account);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  };
+
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Make sure you have Metamask!');
+        return;
+      }
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      console.log(signer);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <div>
+      <p>Please connect using your Metamask wallet</p>
+      <button onClick={connectWallet}>Connect</button>
+    </div>
+  );
+
+  const renderConnectedContainer = () => (
+    <div>
+      <p>You are now connected! :)</p>
+
+    </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Miniflix!</h1>
+      {currentAccount === "" ? renderNotConnectedContainer() : renderConnectedContainer()}
     </div>
   );
 }
