85e5c700bc5f9ce7cdb844a480c3428c7d5b9790
leovct <l009.vincent@gmail.com>
2022-02-06 21:20:50 +0100

    fix: add card to the database

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,9 @@ import MiniflixSubscriptionCard from './contracts/MiniflixSubscriptionCards.json
 
 const aggregatorV3InterfaceABI = [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "description", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint80", "name": "_roundId", "type": "uint80" }], "name": "getRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "latestRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
 const CHAINLINK_MATIC_USD_PRICE_FEED_ADDRESS = "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada";
-const NFT_CONTRACT_ADDRESS = "0x1e9162F3cf08E7E6EE1f67FD35477a7F9aBC87b5";
+const NFT_CONTRACT_ADDRESS = "0x23B822fdc8cc12ccDbB0cDc307000e715174c644";
+
+const API_URL = "https://miniflix-api.herokuapp.com/cards/";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState('');
@@ -107,17 +109,20 @@ const App = () => {
         .then((roundData) => {
             // Do something with roundData
             let maticUSD = roundData.answer / 10**8;
-            subscriptionMaticPrice = 10**18 * Math.round(subscriptionUSDPrice / maticUSD);
+            subscriptionMaticPrice =  Math.round(subscriptionUSDPrice / maticUSD);
             console.log("MATIC/USD price =", maticUSD);
-            console.log("Subscription price: $", subscriptionUSDPrice, "=", subscriptionMaticPrice, "MATIC (WEI)");
+            console.log("Subscription price: $", subscriptionUSDPrice, "=", 1000000000000000000 * subscriptionMaticPrice, "MATIC (WEI)");
         });
 
       //const overrides = { value: ethers.BigNumber.from(subscriptionMaticPrice) };
       let txn = await nftContract.mint(1, { value: subscriptionMaticPrice });
       alert('Minting, please wait...');
-      console.log(txn);
       await txn.wait();
       alert('NFT minted!');
+
+      // create the nft in the database
+      fetch(API_URL + '?duration=30&tier=Basic', {method: 'POST'})
+        .then(resp => console.log(resp));
     } catch (error) {
       console.log(error);
       return;
